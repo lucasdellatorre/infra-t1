af6d3d55d15536dfab74a0e1365801866be1b598
Lucas Dellatorre <lucasdellatorre222@gmail.com>
2024-04-03 01:50:19 -0300

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -19,12 +19,28 @@ WHERE
     AND
     COUNTRY = 'BRAZIL';
     
+EXPLAIN PLAN FOR
+SELECT
+    ap.FIRSTNAME || ' ' || ap.LASTNAME  as FULLNAME,
+    apd.BIRTHDATE,
+    apd.CITY
+FROM
+    AIR_PASSENGERS ap
+    JOIN AIR_PASSENGERS_DETAILS apd ON ap.PASSENGER_ID = apd.PASSENGER_ID
+WHERE
+    SEX = 'w'
+    AND 
+    birthdate <= ADD_MONTHS(SYSDATE,-41*12)
+    AND
+    COUNTRY = 'BRAZIL';
+    
 DROP INDEX sex_idx;
 DROP INDEX country_idx;
 DROP INDEX bday_idx;
 DROP INDEX fk_passenger_id_idx;
 
 DROP CLUSTER cl_passenger_details including tables CASCADE CONSTRAINTS;
+DROP CLUSTER air_passengers_country including tables CASCADE CONSTRAINTS;
 
 ALTER TABLE AIR_PASSENGERS
 DROP PRIMARY KEY;
@@ -37,5 +53,45 @@ CREATE INDEX sex_idx ON AIR_PASSENGERS_DETAILS(sex);
 CREATE INDEX country_idx ON AIR_PASSENGERS_DETAILS(country);
 CREATE INDEX bday_idx ON AIR_PASSENGERS_DETAILS(birthdate);
 
+CREATE TABLE AIR_PASSENGERS_DETAILS(
+    PASSENGER_ID NUMERIC(12) NOT NULL ENABLE,
+    BIRTHDATE DATE NOT NULL enable,
+    SEX CHAR(1) NOT NULL enable,
+    STREET VARCHAR(100) NOT NULL enable,
+    CITY VARCHAR(100) NOT NULL enable,
+    ZIP NUMBER(5) NOT NULL enable,
+    COUNTRY VARCHAR(50) NOT NULL,
+    EMAILADDRESS VARCHAR(120),
+    TELEPHONENO VARCHAR(30)
+) CLUSTER air_passengers_country(country);
+
+CREATE CLUSTER air_passengers_country (
+ country varchar(50)
+)
+INDEX
+SIZE 160;
+
+CREATE INDEX idx_air_passengers_country ON CLUSTER air_passengers_country;
+
+CREATE INDEX cl_passenger_details_idx ON CLUSTER cl_passenger_details;
+
+DROP TABLE AIR_PASSENGERS_DETAILS;
+
 SELECT PLAN_TABLE_OUTPUT 
 FROM TABLE(DBMS_XPLAN.DISPLAY());
+
+SELECT
+  'create table ' || table_name || ' as select * from ARRUDA.' || table_name || ';'
+FROM
+  all_tables
+WHERE
+    owner = 'ARRUDA'
+    AND table_name LIKE 'AIR_%';
+
+
+INSERT INTO AIR_PASSENGERS_DETAILS select * from ARRUDA.AIR_PASSENGERS_DETAILS;
+
+SELECT * FROM AIR_PASSENGERS_DETAILS;
+
+create table AIR_PASSENGERS_DETAILS as select * from ARRUDA.AIR_PASSENGERS_DETAILS;
+
