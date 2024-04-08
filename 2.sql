2145148ef2314e2f8e51e2565b83bcf6b556089e
Lucas Dellatorre <lucasdellatorre222@gmail.com>
2024-04-08 20:52:25 -0300

    Update 2.sql

--- 2.sql
+++ 2.sql
@@ -5,6 +5,10 @@
     que saem E chegam em aeroportos localizados no país 'BRAZIL'. 
 */
 
+/*
+    Adicionar chaver primárias e estrangeiras em todas as tabelas pra ver a diferença
+*/
+
 EXPLAIN PLAN FOR
 SELECT 
     airlines.AIRLINE_NAME,
@@ -20,6 +24,29 @@ FROM
     JOIN AIR_AIRPORTS_GEO_CL airportsgeo ON airportsgeo.AIRPORT_ID = airports.AIRPORT_ID
 WHERE
     airportsgeo.COUNTRY = 'BRAZIL';
+    
+ALTER TABLE AIR_AIRLINES
+ADD PRIMARY KEY(airline_id);
+
+ALTER TABLE AIR_AIRPLANES
+ADD CONSTRAINT fk_airline_id
+FOREIGN KEY (airline_id)
+REFERENCES AIR_AIRLINES(airline_id);
+
+ALTER TABLE AIR_FLIGHTS
+ADD PRIMARY KEY(flight_id);
+
+ALTER TABLE AIR_FLIGHTS
+ADD CONSTRAINT fk_airplane_id
+FOREIGN KEY (airplane_id)
+REFERENCES AIR_AIRPLANES(airplane_id);
+
+ALTER TABLE AIR_AIRPORTS
+ADD PRIMARY KEY(airport_id);
+
+ALTER TABLE AIR_AIRPORTS_GEO_CL
+ADD PRIMARY KEY(airport_id);
+
 
 DESCRIBE air_airports_geo;
 
@@ -40,3 +67,29 @@ CREATE TABLE AIR_AIRPORTS_GEO_CL (
 
 INSERT INTO AIR_AIRPORTS_GEO_CL select * from AIR_AIRPORTS_GEO;
 
+SELECT PLAN_TABLE_OUTPUT 
+FROM TABLE(DBMS_XPLAN.DISPLAY());
+
+ALTER TABLE AIR_AIRPORTS
+ADD PRIMARY KEY(AIRPORT_ID);
+
+ALTER TABLE AIR_FLIGHTS
+ADD CONSTRAINT fk_airport_id_from
+FOREIGN KEY (from_airport_id)
+REFERENCES AIR_AIRPORTS(airport_id);
+
+ALTER TABLE AIR_FLIGHTS
+ADD CONSTRAINT fk_airport_id_to
+FOREIGN KEY (to_airport_id)
+REFERENCES AIR_AIRPORTS(airport_id);
+
+ALTER TABLE AIR_AIRPLANE_TYPES
+ADD PRIMARY KEY(AIRPLANE_TYPE_ID);
+
+ALTER TABLE AIR_AIRPLANES
+ADD CONSTRAINT fk_airplane_type_id
+FOREIGN KEY (AIRPLANE_TYPE_ID)
+REFERENCES AIR_AIRPLANE_TYPES(AIRPLANE_TYPE_ID);
+
+CREATE INDEX fk_airport_id_from ON AIR_FLIGHTS(from_airport_id);
+CREATE INDEX fk_airport_id_to ON AIR_FLIGHTS(to_airport_id);
