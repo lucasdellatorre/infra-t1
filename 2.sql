64345db8ec649877b27f1a64fe950a5c596ebc70
Lucas Dellatorre <lucasdellatorre222@gmail.com>
2024-04-08 20:10:18 -0300

    Update 2.sql

--- 2.sql
+++ 2.sql
@@ -5,20 +5,20 @@
     que saem E chegam em aeroportos localizados no país 'BRAZIL'. 
 */
 
+EXPLAIN PLAN FOR
 SELECT 
     airlines.AIRLINE_NAME,
     airplanes.AIRPLANE_ID,
     airplanest.NAME as AIRPLANE_NAME_TYPE,
     airflights.FLIGHTNO as FLIGHT_NUMBER
 FROM
-    ARRUDA.AIR_AIRLINES airlines    
-    JOIN ARRUDA.AIR_AIRPLANES airplanes ON airplanes.AIRLINE_ID = airlines.AIRLINE_ID
-    JOIN ARRUDA.AIR_AIRPLANE_TYPES airplanest ON airplanest.AIRPLANE_TYPE_ID = airplanes.AIRPLANE_TYPE_ID
-    JOIN ARRUDA.AIR_FLIGHTS airflights ON airflights.AIRPLANE_ID = airplanes.AIRPLANE_ID
-    JOIN ARRUDA.AIR_AIRPORTS airports ON ((airports.AIRPORT_ID = airflights.FROM_AIRPORT_ID) or (airports.AIRPORT_ID = airflights.TO_AIRPORT_ID))
-    JOIN ARRUDA.AIR_AIRPORTS_GEO airportsgeo ON airportsgeo.AIRPORT_ID = airports.AIRPORT_ID
+    AIR_AIRLINES airlines    
+    JOIN AIR_AIRPLANES airplanes ON airplanes.AIRLINE_ID = airlines.AIRLINE_ID
+    JOIN AIR_AIRPLANE_TYPES airplanest ON airplanest.AIRPLANE_TYPE_ID = airplanes.AIRPLANE_TYPE_ID
+    JOIN AIR_FLIGHTS airflights ON airflights.AIRPLANE_ID = airplanes.AIRPLANE_ID
+    JOIN AIR_AIRPORTS airports ON ((airports.AIRPORT_ID = airflights.FROM_AIRPORT_ID) or (airports.AIRPORT_ID = airflights.TO_AIRPORT_ID))
+    JOIN AIR_AIRPORTS_GEO_CL airportsgeo ON airportsgeo.AIRPORT_ID = airports.AIRPORT_ID
 WHERE
     airportsgeo.COUNTRY = 'BRAZIL';
-
 
 
